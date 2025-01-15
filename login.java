3a18ad1b1f4f5d6bbe1508d572a6fa540b011b11
shaikh <siiid7240@gmail.com>
2025-01-15 05:17:51 +0000

    git conflict file

---
+++ login.java
@@ -0,0 +1,12 @@
+public class login
+{
+        public static void main (string args[])
+        {
+                int a=10;
+        for(i=10;i<=10;i++)
+        {
+                system.out.println("print: " i)
+        }
+        }
+}
+
