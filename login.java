ce4f51009cb7065165da97fc779b6ef5df14873b
shaikh <siiid7240@gmail.com>
2025-01-15 05:20:05 +0000

    made 10 to 20

--- login.java
+++ login.java
@@ -2,7 +2,7 @@ public class login
 {
         public static void main (string args[])
         {
-                int a=10;
+                int a=20;
         for(i=10;i<=10;i++)
         {
                 system.out.println("print: " i)
