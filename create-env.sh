2cb00a7e5649e3a98e0dca9f2e00c8a6df50ffc8
shaikh <siiid7240@gmail.com>
2025-02-21 06:52:56 +0530

    new

--- create-env.sh
+++ create-env.sh
@@ -4,7 +4,7 @@ echo "creating $cont_count containers.."
 sleep 2;
 for i in `seq $cont_count`
 do
-0	echo "=============================="
+	echo "=============================="
     echo "Creating tomcat-server$i container.."
     sleep 1
     docker run -it -d --name tomcat-server$i admin27012/docker-img
